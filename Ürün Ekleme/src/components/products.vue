b7d2cf8164036fea9b6625488ea192357409deb5
Ismailakca <ismaillakkca@gmail.com>
2021-01-14 22:45:40 +0300

    ürün ekleme

---
+++ Ürün Ekleme/src/components/products.vue
@@ -0,0 +1,49 @@
+<template>
+ <div v-if="productList.length > 0">
+   <h3 class="text-center">Eklenen Ürünlerin Listesi</h3>
+   <hr>
+   <div class="row product-container">
+
+     <app-product v-for="product in productList">
+       <img class="card-img-top" :src="product.selectedImage" :alt="product.title">
+       <div class="card-body">
+         <h5 class="card-title">{{product.title}}</h5>
+         <small><strong>Adet : </strong> {{product.count}}</small>
+         <br>
+         <small><strong>Fiyat : </strong> {{product.price}}</small>
+         <br>
+         <small><strong>Tutar : </strong> {{product.totalPrice}}</small>
+       </div>
+     </app-product>
+   </div>
+ </div>
+
+</template>
+
+<script>
+  import Prodcut from "./product";
+  import {eventBus} from "../main";
+
+
+  export default {
+    components : {
+      appProduct : Prodcut,
+    },
+    data(){
+      return {
+        productList: [],
+      }
+    },
+    created(){
+      if (this.productList.length < 2){
+          eventBus.$on("productEklendi", (product) => {
+          this.productList.push(product);
+          eventBus.$emit("progressBarUpdated",this.productList.length);
+        })
+      }
+      else {
+        alert("Daha fazla ürün ekleyemezsiniz");
+      }
+    }
+  }
+</script>
---
+++ Ürün Ekleme/src/components/progress.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="progress">
+    <div class="progress-bar bg-info" role="progressbar" :style="{width: currentwidth + '%'}" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
+      {{currentItem}} / 10
+    </div>
+
+  </div>
+</template>
+
+<script>
+  import {eventBus} from "../main";
+
+  export  default {
+    data(){
+      return{
+        currentItem : 0,
+        currentwidth : 0,
+      }
+    },
+    created(){
+      eventBus.$on("progressBarUpdated", (current) => {
+        this.currentItem = current;
+        this.currentwidth = current * 10;
+      })
+    }
+  }
+</script>
